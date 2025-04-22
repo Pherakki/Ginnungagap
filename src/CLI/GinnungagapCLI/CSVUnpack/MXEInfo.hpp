a9cd3fcc58448fdfc83aeac0ce4ba5fed4e83540
akorov <korovf@gmail.com>
2025-04-22 11:51:01 +0300

    Implement --add-entity feature

--- src/CLI/GinnungagapCLI/CSVUnpack/MXEInfo.hpp
+++ src/CLI/GinnungagapCLI/CSVUnpack/MXEInfo.hpp
@@ -16,7 +16,24 @@ struct ParameterErrorData
     {}
 };
 
+struct EntityErrorData
+{
+    uint32_t ID;
+    uint32_t row;
+    uint32_t csv_idx;
+
+    EntityErrorData(uint32_t ID, uint32_t row, uint32_t csv_idx)
+        : ID{ ID }
+        , row{ row }
+        , csv_idx{ csv_idx }
+    {}
+};
+
 std::vector<ParameterErrorData> checkParameterTableCSVErrors(const std::string& params_root);
 std::vector<ParameterErrorData> checkParameterTableCSVErrors(const std::filesystem::path& params_root);
 
-uint32_t nextAvailableID(const std::filesystem::path& root_path);
+std::vector<EntityErrorData> checkEntityTableCSVErrors(const std::string& entities_root);
+std::vector<EntityErrorData> checkEntityTableCSVErrors(const std::filesystem::path& entities_root);
+
+uint32_t nextAvailableParamID(const std::filesystem::path& root_path);
+uint32_t nextAvailableEntityID(const std::filesystem::path& root_path);
